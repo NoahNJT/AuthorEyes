3ccbfa6f422fdc8683fd0475ccf057ae6919dea8
Noah True <njt3d@mtmail.mtsu.edu>
2025-08-02 10:52:35 -0500

    Character Map Graph Implementation Work

--- main.py
+++ main.py
@@ -18,25 +18,36 @@ character_frame_layout = [[sg.OptionMenu(character_list),
                            sg.Input(), 
                            sg.ColorChooserButton("Select Color")]]
 
-character_tab = [[sg.Graph((1000, 400),(0, 0),(1000, 400), background_color="white", key = "-CMAP-")], 
-                 [sg.Frame('Characters', character_frame_layout)]]
+char_col = [[sg.Text('Choose what clicking/dragging does:', enable_events=True)],
+            [sg.Radio('Draw Line', 1, key='-LINE2-', enable_events=True), 
+             sg.Input("#000000", size = (7, None), key = "-LINECOL-"),
+             sg.ColorChooserButton(button_text = "Line Color")],
+            [sg.Radio('Erase Item', 1, key='-ERASE2-', enable_events=True)],
+            [sg.Radio('Erase All', 1, key='-CLEAR2-', enable_events=True)],
+            [sg.Radio('Move All Items', 1, key='-MOVEALL2-', enable_events=True)],
+            [sg.Radio('Move Item', 1, True, key='-MOVE2-', enable_events=True)],
+            [sg.Input("Character Name", key="-POINTSTRING2-")],
+            [sg.Button("Add Character", key="-PLOTPOINT2-"), sg.Button("Save")]]
+
+character_tab = [[sg.Graph((720, 400),(0, 0),(720, 400), background_color="white", key = "-CHARMAP-", change_submits=True, drag_submits=True), sg.Col(char_col)]]
 
 # MAYBE: try to do a plot points in a container element with extend layout/make invisible; try text box with an update button
 col = [[sg.Text('Choose what clicking/dragging does:', enable_events=True)],
-       [sg.Radio('Draw Rectangle', 1, key='-RECT-', enable_events=True)],
        [sg.Radio('Draw Line', 1, key='-LINE-', enable_events=True)],
        [sg.Radio('Erase Item', 1, key='-ERASE-', enable_events=True)],
-       [sg.Radio('Erase All', 1, key='-CLEAR-', enable_events=True)],
+       [sg.Radio('Erase All (INCLUDES DIAGRAM!)', 1, key='-CLEAR-', enable_events=True)],
        [sg.Radio('Move All Items', 1, key='-MOVEALL-', enable_events=True)],
-       [sg.Radio('Move Item', 1, True, key='-MOVE-', enable_events=True)]]
+       [sg.Radio('Move Item', 1, True, key='-MOVE-', enable_events=True)],
+       [sg.Multiline("Write a plot point here!",size = (None, 10), key="-POINTSTRING-")],
+       [sg.Button("Add Plot Point", key="-PLOTPOINT-"), sg.Button("Save")]]
 
-plot_tab = [[sg.Graph((720, 400),(0, 0),(720, 400), background_color="white", key = "-DIAGRAM-", change_submits=True, drag_submits=True), sg.Col(col)], 
-            [sg.Button("Add Plot Point", key="-PLOTPOINT-"), sg.Multiline(key="-POINTSTRING-")]]
+plot_tab = [[sg.Graph((720, 400),(0, 0),(720, 400), background_color="white", key = "-DIAGRAM-", change_submits=True, drag_submits=True), sg.Col(col)]]
 
 layout = [[sg.TabGroup([[sg.Tab("Chapter Planner", chapter_tab)], [sg.Tab("Character Map", character_tab)], [sg.Tab("Plot Diagram", plot_tab)]])]]
 
 window = sg.Window("AuthorEyes", layout, size=(1000,500))
 window.finalize()
+# ADD CLIMAX TEXT
 window["-DIAGRAM-"].draw_text("Exposition", (100, 110), font = ('Arial', 8))
 window["-DIAGRAM-"].draw_text("Rising Action", (260, 240), font = ('Arial', 8))
 window["-DIAGRAM-"].draw_text("Falling Action", (530, 290), font = ('Arial', 8))
@@ -49,6 +60,7 @@ window["-DIAGRAM-"].draw_line((550, 220), (700, 220), width = 2)
 
 dragging = False
 graph = window["-DIAGRAM-"]
+map_graph = window["-CHARMAP-"]
 start_point = end_point = prior_rect = None
 graph.bind('<Button-3>', '+RIGHT+')
 
@@ -89,8 +101,6 @@ while True:
                 for fig in drag_figures:
                     graph.move_figure(fig, delta_x, delta_y)
                     graph.update()
-            elif values['-RECT-']:
-                prior_rect = graph.draw_rectangle(start_point, end_point)
             elif values['-LINE-']:
                 prior_rect = graph.draw_line(start_point, end_point, width = 2)
             elif values['-ERASE-']:
@@ -107,5 +117,39 @@ while True:
 
     if event == "-PLOTPOINT-":
         graph.draw_text(values["-POINTSTRING-"], (100, 100), font = ('Arial', 8))
+
+
+    if event == "-CHARMAP-":
+        x, y = values["-CHARMAP-"]
+        if not dragging:
+            start_point = (x, y)
+            dragging = True
+            drag_figures = map_graph.get_figures_at_location((x,y))
+            lastxy = x, y
+        else:
+            end_point = (x, y)
+        if prior_rect:
+            map_graph.delete_figure(prior_rect)
+        delta_x, delta_y = x - lastxy[0], y - lastxy[1]
+        lastxy = x,y
+        if None not in (start_point, end_point):
+            if values['-MOVE2-']:
+                for fig in drag_figures:
+                    map_graph.move_figure(fig, delta_x, delta_y)
+                    map_graph.update()
+            elif values['-LINE2-']:
+                prior_rect = map_graph.draw_line(start_point, end_point, width = 2, color = values["-LINECOL-"])
+            elif values['-ERASE2-']:
+                for figure in drag_figures:
+                    map_graph.delete_figure(figure)
+            elif values['-CLEAR2-']:
+                map_graph.erase()
+            elif values['-MOVEALL2-']:
+                map_graph.move(delta_x, delta_y)
+    elif event.endswith('+UP'):  # The drawing has ended because mouse up
+        start_point, end_point = None, None  # enable grabbing a new rect
+        dragging = False
+        prior_rect = None
+
 
 window.close()
